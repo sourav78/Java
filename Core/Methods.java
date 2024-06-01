744a98a71c8a3432945fb1b66253a569d08e7493
sourav78 <souravranjan488@gmail.com>
2024-06-01 16:24:12 +0530

    Methods in java

---
+++ Core/Methods.java
@@ -0,0 +1,24 @@
+public class Methods {
+    public static void main(String[] args) {
+        //Methods
+
+        canGreet();
+
+        System.out.println(add2Num(5, 8));
+
+        System.out.println(Math.min(2, 7));
+
+    }
+
+    
+    public static void canGreet(){
+        System.out.println("Hello Methods");
+        
+    }
+
+    public static int add2Num(int a , int b){
+        return a+b;
+    }
+
+
+}
