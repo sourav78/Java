a273730a2e415174fd1c7f69e0db5ab7ca945501
sourav78 <souravranjan488@gmail.com>
2024-06-03 16:03:08 +0530

    Classes in java

---
+++ Core/OOPS1.java
@@ -0,0 +1,17 @@
+public class OOPS1 {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.name = "GTR";
+        car1.run();
+    }
+}
+
+class Car{
+    String name;
+    int whele;
+    int speed;
+
+    public void run(){
+        System.out.println(name+" is running.");
+    }
+}
