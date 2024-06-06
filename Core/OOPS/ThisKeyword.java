31483e672811196a9e1036895906bc15aeef7f3f
sourav78 <souravranjan488@gmail.com>
2024-06-06 15:47:59 +0530

    This keyword in java

---
+++ Core/OOPS/ThisKeyword.java
@@ -0,0 +1,27 @@
+package OOPS;
+
+public class ThisKeyword {
+    public static void main(String[] args) {
+
+        Algo algo = new Algo(2, 3);
+        algo.add();
+
+    }
+}
+
+class Algo{
+    int num1;
+    int num2;
+
+    public Algo(int num1, int num2){
+        this.num1 = num1;
+        this.num2 = num2;
+
+        this.add();
+//        System.out.println(this); //OOPS.Algo@65ab7765
+    }
+
+    public void add(){
+        System.out.println("Sum is: "+(num1+num2));
+    }
+}
