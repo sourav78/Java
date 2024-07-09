8b9e8fcd6fb8b5dbc3fc0a377552eb8bff368dea
sourav78 <souravranjan488@gmail.com>
2024-07-09 11:32:36 +0530

    DSa - insertionsort bug fix

--- DSA/InsertionSort.java
+++ DSA/InsertionSort.java
@@ -22,16 +22,17 @@ public class InsertionSort {
             int temp = arr[j];
             arr[j] = arr[j - 1];
             arr[j - 1] = temp;
+            j--;
         }
 
         recursiveInsertionSort(arr, i+1, n);
     }
 
     public static void main(String[] args) {
-        int[] arr = { 35, 76, 13, 26, 37, 90, 65 };
+        int[] arr = {35, 76, 13, 26, 37, 90, 65, 78, 3, 6, 87, 45, 32, 8, 90, 56, 24, 65, 34};
 
         // insertionSort(arr);
-        recursiveInsertionSort(arr, 0, 7);
+        recursiveInsertionSort(arr, 0, arr.length);
 
         System.out.println(Arrays.toString(arr));
     }
