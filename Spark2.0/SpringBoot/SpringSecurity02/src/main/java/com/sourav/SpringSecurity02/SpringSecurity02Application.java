ab0e231bfb6a252767be80a5f843b3eccf50406a
sourav78 <souravranjan488@gmail.com>
2024-10-21 07:57:40 +0530

    Spring security - 02

---
+++ Spark2.0/SpringBoot/SpringSecurity02/src/main/java/com/sourav/SpringSecurity02/SpringSecurity02Application.java
@@ -0,0 +1,13 @@
+package com.sourav.SpringSecurity02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity02Application.class, args);
+	}
+
+}
