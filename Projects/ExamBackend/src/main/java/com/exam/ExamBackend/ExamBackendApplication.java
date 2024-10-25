3afffc22008b4dc2c2b77a77677398e1c96a4773
sourav78 <souravranjan488@gmail.com>
2024-10-25 08:31:49 +0530

    ExamPortal - Project created and Database setup

---
+++ Projects/ExamBackend/src/main/java/com/exam/ExamBackend/ExamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.exam.ExamBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamBackendApplication.class, args);
+	}
+
+}
